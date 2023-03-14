a73654a3e478b5f5e6da39c900e3ed7a7002a08e
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-03-14 10:36:36 +0100

    Update UserDetailsImpl

--- Anti-Fraud System/task/src/antifraud/security/UserDetailsImpl.java
+++ Anti-Fraud System/task/src/antifraud/security/UserDetailsImpl.java
@@ -1,22 +1,27 @@
 package antifraud.security;
 
+import antifraud.model.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+
 /**
  * This class implements the UserDetails interface and is used by Spring Security.
  */
 public class UserDetailsImpl implements UserDetails {
-//    private final String username;
-//    private final String password;
-//    private final List<GrantedAuthority> rolesAndAuthorities;
-//    public UserDetailsImpl(User user) {
-//        this.username = user.getEmail();
-//        this.password = user.getPassword();
-//        List<String> recipesAuthoredList= new ArrayList<>();
-//        this.rolesAndAuthorities = List.of(new SimpleGrantedAuthority(recipesAuthoredList.toString()));
-//    }
+    private final String username;
+    private final String password;
+    private final List<GrantedAuthority> rolesAndAuthorities;
+    public UserDetailsImpl(User user) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        List<String> recipesAuthoredList= new ArrayList<>();
+        this.rolesAndAuthorities = List.of(new SimpleGrantedAuthority(recipesAuthoredList.toString()));
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
