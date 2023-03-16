3b3a4f5f6c0844c4bdb02347f0981349e434be02
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-03-16 15:40:53 +0100

    Modify

--- Anti-Fraud System/task/src/antifraud/persistance/UserRepository.java
+++ Anti-Fraud System/task/src/antifraud/persistance/UserRepository.java
@@ -4,10 +4,14 @@ import antifraud.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Interface is used to interact with the user table in the database.
  */
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsernameIgnoreCase(String username);
+    Optional<User> findByUsernameIgnoreCase(String username);
+    List<User> findAllByOrderById();
 }
