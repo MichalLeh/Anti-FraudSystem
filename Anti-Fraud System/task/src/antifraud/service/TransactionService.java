3ba8e18a0d22e41a60356a3eb594599a8967a393
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-03-16 15:31:42 +0100

    Modify

--- Anti-Fraud System/task/src/antifraud/service/TransactionService.java
+++ Anti-Fraud System/task/src/antifraud/service/TransactionService.java
@@ -1,17 +1,34 @@
 package antifraud.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class TransactionService {
     public TransactionService() {
     }
-    public String checkTransaction(Long amount) {
-        if (amount > 1500) {
-            return "PROHIBITED";
-        } else if (amount > 200) {
-            return "MANUAL_PROCESSING";
+    /**
+     * Process a transaction.
+     *
+     * @param amount Transaction to be processed
+     * @return A response entity with the status of the transaction
+     */
+    public ResponseEntity processTransaction(Long amount) {
+        try {
+            if (amount <= 0) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            } else if (amount <= 200) {
+                return ResponseEntity.status(HttpStatus.OK).body(Map.of("result", "ALLOWED"));
+            } else if (amount <= 1500) {
+                return ResponseEntity.status(HttpStatus.OK).body(Map.of("result", "MANUAL_PROCESSING"));
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(Map.of("result", "PROHIBITED"));
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return "ALLOWED";
     }
 }
