e258b4ef90630d82646feea175cfb282715eae35
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-03-16 15:08:29 +0100

    Modify

--- Anti-Fraud System/task/src/antifraud/presentation/UserController.java
+++ Anti-Fraud System/task/src/antifraud/presentation/UserController.java
@@ -1,32 +1,68 @@
 package antifraud.presentation;
 
 import antifraud.model.User;
-import antifraud.service.AuthenticationService;
-import org.springframework.http.HttpStatus;
+import antifraud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api/auth")
 public class UserController {
-    private AuthenticationService authenticationService;
-    private PasswordEncoder encoder;
-    public UserController(AuthenticationService authenticationService, PasswordEncoder encoder) {
-        this.authenticationService = authenticationService;
-        this.encoder = encoder;
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Add user.
+     *
+     * @param user User to be added to the database
+     * @return A response entity with the status of the addition
+     */
+    @PostMapping("/user")
+    public ResponseEntity postUser(@RequestBody @Valid User user) {
+        return userService.add(user);
     }
-    @PostMapping("/api/auth/user")
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity registerUser(@RequestBody @Valid User user) {
-        if (!authenticationService.checkIfUserExists(user.getUsername())) {
-            authenticationService.registerUser(user);
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        }
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    /**
+     * Get user(s).
+     *
+     * @return A list of all users in database
+     */
+    @GetMapping("/list")
+    public List<User> getUser(){
+        return userService.findAll();
+    }
+    /**
+     * Delete user.
+     *
+     * @param username User to be deleted
+     * @return A response entity with the status of the deletion
+     */
+    @DeleteMapping("/user/{username}")
+    public ResponseEntity deleteUser(@PathVariable("username") String username){
+        return userService.deleteUser(username);
+    }
+    /**
+     * Change of user's role.
+     *
+     * @param request JSON body containing username and role
+     * @return A response entity with the status of the role change
+     */
+    @PutMapping("/role")
+    public ResponseEntity<?> changeRole(@RequestBody @Valid Map<String, String> request) {
+        return userService.changeRole(request);
+    }
+    /**
+     * Change of user's access.
+     *
+     * @param request JSON body containing username and operation
+     * @return A response entity with the status of the access change
+     */
+    @PutMapping("/access")
+    public ResponseEntity<?> changeAccess(@RequestBody @Valid Map<String, String> request) {
+        return userService.changeAccess(request);
     }
 }
