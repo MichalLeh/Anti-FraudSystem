96a2704f82198109075de0b4a52586b692f2da64
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-03-16 15:39:32 +0100

    Modify

--- Anti-Fraud System/task/src/antifraud/security/UserDetailsImpl.java
+++ Anti-Fraud System/task/src/antifraud/security/UserDetailsImpl.java
@@ -1,11 +1,11 @@
 package antifraud.security;
 
+import antifraud.model.ERole;
 import antifraud.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -16,38 +16,40 @@ public class UserDetailsImpl implements UserDetails {
     private final String username;
     private final String password;
     private final List<GrantedAuthority> rolesAndAuthorities;
+    private final boolean isAccountNonLocked;
     public UserDetailsImpl(User user) {
         this.username = user.getUsername();
         this.password = user.getPassword();
-        List<String> recipesAuthoredList= new ArrayList<>();
-        this.rolesAndAuthorities = List.of(new SimpleGrantedAuthority(recipesAuthoredList.toString()));
+        rolesAndAuthorities = List.of(new SimpleGrantedAuthority(ERole.valueOfLabel(user.getRole()).toString()));
+        isAccountNonLocked = user.isAccountNonLocked();
     }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return rolesAndAuthorities;
     }
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
     @Override
     public String getUsername() {
-        return null;
-    }
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
+        return username;
     }
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return isAccountNonLocked;
+    }
+    //UNUSED METHODS BELOW
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
