e4b69269942e21df430a7cf9e854c8b65bfd5330
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-03-20 09:43:05 +0100

    Create new class Card

---
+++ Anti-Fraud System/task/src/antifraud/model/Card.java
@@ -0,0 +1,23 @@
+package antifraud.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name="CARDS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Card {
+    @Id
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotEmpty
+    private String number;
+}
