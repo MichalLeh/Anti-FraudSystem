5bc2f51882f5ffe0ebba8cb07106a975ce42187a
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-03-21 09:58:03 +0100

    Modify class

--- Anti-Fraud System/task/src/antifraud/model/Transaction.java
+++ Anti-Fraud System/task/src/antifraud/model/Transaction.java
@@ -1,24 +1,24 @@
 package antifraud.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transaction {
     @NotNull
+    @Min(1)
     private Long amount;
-    public Transaction(){
-    }
-    public Transaction(Long amount) {
-        this.amount = amount;
-    }
-//    public Long getAmount() {
-//        return amount;
-//    }
-//    public void setAmount(Long amount) {
-//        this.amount = amount;
-//    }
+    @NotEmpty
+    private String ip;
+    @NotEmpty
+    private String number;
 }
